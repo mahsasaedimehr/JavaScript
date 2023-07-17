6f3c5a03018881ec4320c1428438ef13540a7e0d
mahsasaedimehr <mahsa.saedimehr@gmail.com>
2023-07-17 21:51:28 +0330

    complated project

---
+++ index.js
@@ -0,0 +1,14 @@
+let num1 = +prompt("Enter number1");
+let num2 = +prompt("Enter number2");
+
+console.log(num1);
+console.log(typeof num1);
+console.log(num2);
+console.log(typeof num2);
+
+let add = (num1 + num2);
+
+console.log(add);
+console.log(typeof add);
+
+document.write("sum = ", add);
