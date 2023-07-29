e80b840e3fcdfcebdb56c5f119f59b637b0e3e39
mahsasaedimehr <mahsa.saedimehr@gmail.com>
2023-07-29 17:03:59 +0330

    fix/mahsa/complated project-4/t-2 and complated readme file content

--- t-2/index-2.js
+++ t-2/index-2.js
@@ -1,19 +1,33 @@
-function search(text, word){
-    var x = 0, y=0;
-    for (i=0; i< text.length; i++){
-        if(text[i] == word[0]){
-            for(j=i; j< i+word.length; j++){
-                if(text[j]==word[j-i]){
-                    y++;
-                }
-                if(y==word.length){
-                    x++;
-                }
-            }
-            y=0;
-        }
+// function search(){
+//     var x = 0, y=0;
+//     for (i=0; i< text.length; i++){
+//         if(text[i] == word[0]){
+//             for(j=i; j< i+word.length; j++){
+//                 if(text[j]==word[j-i]){
+//                     y++;
+//                 }
+//                 if(y==word.length){
+//                     x++;
+//                 }
+//             }
+//             y=0;
+//         }
+//     }
+//     return "'"+word+"' was found "+x+" times.";
+// }
+// console.log(search('the quick brown fox','fox'));
+// console.log(search("error"));
+
+function search(){
+    const wordsearch = prompt("enter your word")
+    const mytext = ("you should search mahsa");
+    const word = (mahsa);
+    if(word==mahsa){
+        return true;
     }
-    return "'"+word+"' was found "+x+" times.";
+    else{
+        return false;
+    }
 }
-console.log(search('the quick brown fox','fox'));
-console.log(search("error"));
+
+console.log(search());
